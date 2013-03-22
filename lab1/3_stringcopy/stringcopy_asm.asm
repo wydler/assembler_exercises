4072af35ce6e70f79799628524bf7174d8b6a370
zwostein <provisorisch@online.de>
2013-03-22 13:24:46 +0100

    lab 1 - 3 stringcopy -> done

--- lab1/3_stringcopy/stringcopy_asm.asm
+++ lab1/3_stringcopy/stringcopy_asm.asm
@@ -1,4 +1,4 @@
-;stringcopy_asm.asm 30.08.2010
+  ;stringcopy_asm.asm 30.08.2010
 ;copy one string (character array) to another
 ;count the number of copied characters
 
@@ -17,11 +17,25 @@ segment .text
 
 stringcopy:
         enter   0,0               ; setup routine
-        pusha			          ; save register
+        pusha         ; save register
 
 ;Please continue
+	mov ecx, 0		;counter
+	mov ebx, buf_in		;ebx points to source
+	mov edx, buf_out	;edx points to destination
+loopy:
+	mov al, [ebx]		;load char from source to accumulator
+	mov [edx], al		;move char to destination
+	inc ebx			;increment both pointers
+	inc edx
+	cmp al, 0		;jump to 'done' if zero char detected
+	jz done
+	inc ecx			;increment counter
+	jmp loopy
 
-     
+done:
+	mov ebx, number		;load pointer to number
+	mov [ebx], ecx		;write number
 ;End of your program
 
 	popa
