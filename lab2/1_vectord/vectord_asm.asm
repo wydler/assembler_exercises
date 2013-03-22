51a4598ce63dae53ceeaea0bcbbeb2dd64780eea
Michael Wydler <michael.wydler@googlemail.com>
2013-03-22 14:09:58 +0100

    Basic implementation. Not working!

--- lab2/1_vectord/vectord_asm.asm
+++ lab2/1_vectord/vectord_asm.asm
@@ -7,47 +7,49 @@ segment .text
 global  vectord_asm
 
 %define dimension	[ebp+8]
-%define y1		[ebp+12]
-%define y2		[ebp+16]
+%define y1				[ebp+12]
+%define y2				[ebp+16]
 %define distance2	[ebp+20]
 
 
 vectord_asm:
-        enter   0,0            ;setup routine
-        pusha			;save register
+  enter   0,0       ;setup routine
+  pusha							;save register
 	
-; Please continue here!
-	mov eax, distance2
-	mov ebx, 0
-	mov [eax], ebx
-	mov eax, y1
-	mov ebx, y2
-	mov ecx, dimension
-loopy:
-	cmp ecx, 0
-	jz done
-	pusha
-	mov ecx, [eax]	;load y1 and y2
-	mov edx, [ebx]
-	add edx, ecx	;edx=y1+y2
-	mov eax, edx	;setup (y1+y2)^2
-	mov ebx, edx
-	imul eax, ebx	;do square -> edx:eax
-	mov ebx, distance2	;load pointer to distance
-	add eax, [ebx]		;add previous distance
-	mov [ebx], eax		;write back
-	popa
-	add eax, 4
-	add ebx, 4
-	dec ecx
-	jmp loopy
-done:
+  mov ebx, y1
+  mov edx, y2
+  mov ecx, dimension
 
-; end of your program
+loopee:
+  cmp ecx,0
+  jz end
 
+  mov eax,[ebx]
+  sub eax,[edx]
+
+  push ebx
+  push ecx
+  push edx
+
+  mov ecx, distance2
+  mov ebx,eax
+  imul ebx
+  add [ecx], eax
+
+  pop edx
+  pop ecx
+  pop ebx
+
+  inc ebx
+  inc edx
+
+  dec ecx
+  jmp loopee
+	                                                                                                                                                             
+end:
 	popa
-        mov     eax, 0          ;return to C
-        leave
+  mov     eax, 0          ;return to C
+  leave
 	ret
 
 error:
