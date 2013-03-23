6e4059ad82cad23923e0534f18cb00ca107922ab
Michael Wydler <michael.wydler@googlemail.com>
2013-03-24 00:27:14 +0100

    Add comments.

--- lab3/1_mean/mean_asm.asm
+++ lab3/1_mean/mean_asm.asm
@@ -15,24 +15,24 @@ mean:
   enter 0,0               ; setup routine
   pusha			              ; save register
 
-  mov eax, 0
-  mov edx, myData
-  mov cx, [edx]
-  mov ebx, 0
+  mov eax, 0              ; set register to 0
+  mov edx, myData         ; set pointer to data
+  mov cx, [edx]           ; get count value
+  mov ebx, 0              ; set register to 0
 
 loopee:
-  add edx, 2
-  mov bx, [edx]
-  add eax, ebx
-  loopnz loopee
+  add edx, 2              ; increment pointer position
+  mov bx, [edx]           ; get value as position
+  add eax, ebx            ; add value to others
+  loopnz loopee           ; loop until counter 0
 
 end:
-  mov ebx, myData
-  mov cx, [ebx]
-  mov edx, 0
-  idiv cx
-  mov ebx, result
-  mov [ebx], eax
+  mov ebx, myData         ; get data pointer
+  mov cx, [ebx]           ; get count value
+  mov edx, 0              ; set register to 0
+  idiv cx                 ; div sum by count
+  mov ebx, result         ; get result pointer
+  mov [ebx], eax          ; save result to pointer
 	popa
   mov eax, 0              ; return back to C
   leave
