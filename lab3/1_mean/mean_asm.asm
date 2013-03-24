77b282a2c9214d709864772cfc6af5c920a2af04
Tobias Himmer <provisorisch@online.de>
2013-03-24 11:56:09 +0100

    lab 3 - 1 mean -> work in progress

--- lab3/1_mean/mean_asm.asm
+++ lab3/1_mean/mean_asm.asm
@@ -7,27 +7,32 @@ segment .bss
 
 segment .text
 
-        global  mean
+	global  mean
 
 %define myData		[ebp+8]
 %define result		[ebp+12]
 
 mean:
-        enter   0,0               ; setup routine
-        pusha			  ; save register
+	enter	0,0	; setup routine
+	pusha		; save register
 
 ;Please continue
-	
-;End of your program 
+
+	mov	edx, myData	; load pointer to struct
+	mov	ax, [edx]	; extract count
+	mov	ebx, result
+	mov	[ebx], ax
+
+;End of your program
 
 	popa
-        mov     eax, 0           ; return back to C
-        leave
+	mov	eax, 0	; return back to C
+	leave
 	ret
 
 error:
 	popa
-        mov     eax, -1           ; return back to C
-        leave
+	mov	eax, -1	; return back to C
+	leave
 	ret
 
