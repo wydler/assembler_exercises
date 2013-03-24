73b21cfd27b4d99e41839de55c355a6d134badb5
Tobias Himmer <provisorisch@online.de>
2013-03-24 13:20:29 +0100

    lab 3 - 1 mean -> done

--- lab3/1_mean/mean_asm.asm
+++ lab3/1_mean/mean_asm.asm
@@ -9,30 +9,53 @@ segment .text
 
 	global  mean
 
-%define myData		[ebp+8]
-%define result		[ebp+12]
+%define myData	[ebp+8]
+%define result	[ebp+12]
 
 mean:
-	enter	0,0	; setup routine
+	enter 0,0	; setup routine
 	pusha		; save register
 
 ;Please continue
 
-	mov	edx, myData	; load pointer to struct
-	mov	ax, [edx]	; extract count
-	mov	ebx, result
-	mov	[ebx], ax
+	mov edx, myData	; load pointer to struct in edx
+	mov bx, [edx]	; extract count to bx
+	add edx, 2	; edx now points to first value
+	mov cx, 0	; reset counter
+	mov ax, 0	; reset sum
+
+	pusha
+loopy:
+		cmp bx, cx	; already looped through all values?
+		je done		; -> exit loop
+		add ax, [edx]	; add value to sum
+		add edx, 2	; set edx to next value
+		inc cx
+		jmp loopy
+done:
+		mov edx, result	; save sum to result
+		mov [edx], ax
+	popa
+	
+	; prepare for division (count already in bx)
+	mov ecx, result	; load result to ax
+	mov ax, [ecx]
+
+	mov dx, 0
+	idiv bx		; divide (dx:ax)/bx = ax (dx is remainder)
+
+	mov [ecx], ax	; write back
 
 ;End of your program
 
 	popa
-	mov	eax, 0	; return back to C
+	mov eax, 0	; return back to C
 	leave
 	ret
 
 error:
 	popa
-	mov	eax, -1	; return back to C
+	mov eax, -1	; return back to C
 	leave
 	ret
 
