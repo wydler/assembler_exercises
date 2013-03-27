1d7add74d8bca7219d4680c50c0547ddb9b9dddd
Michael Wydler <michael.wydler@googlemail.com>
2013-03-27 10:19:59 +0100

    Add comments and some small fix.

--- lab3/2_convertbin/convbin_asm.asm
+++ lab3/2_convertbin/convbin_asm.asm
@@ -2,7 +2,7 @@ segment .data
 segment .bss
 segment .text
 
-        global  convbin
+global  convbin
 
 %define result		[ebp+12]
 %define value_a		[ebp+8]
@@ -11,11 +11,10 @@ convbin:
 	enter 0,0	; setup routine
 	pusha		; save register
 
-;Please continue!
 	mov edx, value_a	; value in edx
 
 	mov ecx, result		; pointer to last char (string shall be reversed)
-	add ecx, 31
+	add ecx, 31			; set pointer to last char
 
 	mov [ecx+1], byte 0	; null termination
 
@@ -23,20 +22,23 @@ loopy:
 	mov eax, edx	; logical 'and' edx to get lowest bit
 	and eax, 1
 	jz isnull
+
 isset:
 	mov [ecx], byte '1'
 	jmp nextbit
+
 isnull:
 	mov [ecx], byte '0'
+	jmp nextbit
+
 nextbit:
 	cmp ecx, result	; exit loop on last bit
 	je done
 	dec ecx		; update pointer to output 'next' char
 	rcr edx, 1	; right shift value
 	jmp loopy
-done:
-;end of your program
 
+done:
 	popa
 	mov eax, 0	; return back to C
 	leave
